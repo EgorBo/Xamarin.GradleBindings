998daeed5054c15277d627a1d02118a5846177ce
Egor Bogatov <egorbo@gmail.com>
2015-06-30 01:53:01 +0300

    add empty XamarinStudio add-in project

---
+++ GradleBindings.XamarinStudio/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"GradleBindings.XamarinStudio", 
+	Namespace = "GradleBindings.XamarinStudio",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("GradleBindings.XamarinStudio")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("GradleBindings.XamarinStudio")]
+[assembly:AddinAuthor ("Egorbo")]
+
