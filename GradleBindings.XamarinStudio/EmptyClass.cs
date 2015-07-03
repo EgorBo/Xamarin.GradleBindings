b43cb28f8b791fdd07b93023e1c87d5a8049628b
Egor Bogatov <egorbo@gmail.com>
2015-07-03 16:53:28 +0300

    update icon

---
+++ GradleBindings.XamarinStudio/EmptyClass.cs
@@ -0,0 +1,35 @@
+﻿using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui;   
+using Mono.TextEditor;
+using System;
+using System.Linq;
+
+namespace GradleBindings.XamarinStudio
+{
+	public class InsertDateHandler : CommandHandler
+	{
+		protected override void Run ()
+		{
+			var templates = IdeApp.Services.TemplatingService.GetProjectTemplateCategories ();
+			var temps = templates.First (t => t.Id == "android").Categories;
+			var pci = new MonoDevelop.Projects.ProjectCreateInformation ();
+			pci.ParentFolder = IdeApp.ProjectOperations.CurrentSelectedSolution.RootFolder;
+			//pci.ProjectBasePath =
+			//pci.ProjectName = 
+
+			IdeApp.Services.ProjectService.CreateProject ("MonoDroidBindingProject", pci, null);
+		}
+
+		protected override void Update (CommandInfo info)
+		{
+
+		}   
+	}    
+
+	public enum DateInserterCommands
+	{
+		InsertDate,
+	}
+}
+
